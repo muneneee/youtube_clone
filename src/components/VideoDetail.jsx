bb5b86e125e616a4e8f259d30ed6f15732739aba
muneneee <lukekevin39@gmail.com>
2023-04-12 12:51:21 +0300

    Added recommended videos

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -2,20 +2,24 @@ import { useState, useEffect } from "react";
 import { Link, useParams } from 'react-router-dom';
 import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
-import { CheckCircle } from "@mui/icons-material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 
-import { Video } from './';
+import { Videos } from './';
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
   const { id } = useParams();
 
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
       .then((data) => setVideoDetail(data.items[0]));
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+      .then((data) => setVideos(data.items));
   }, [id]);
 
   if (!videoDetail?.snippet) {
@@ -42,7 +46,7 @@ const VideoDetail = () => {
             <Link to={`/channel/${channelId}`}>
               <Typography variant={{ sm: 'subtitle1', md: 'h6' }} color="#fff">
                 {channelTitle}
-                <CheckCircle sx={{ fontSize: '12px', color: 'gray', ml: '5px'}} />
+                <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
               </Typography>
             </Link>
             <Stack direction="row" gap="20px" alignItems="center">
@@ -55,6 +59,9 @@ const VideoDetail = () => {
             </Stack>
           </Stack>
         </Box>
+      </Box>
+      <Box px={2} py={{md: 1, xs: 5}} justifyContent="center" alignItems="center">
+          <Videos videos={videos} direction="column" />
       </Box>
     </Stack>
    </Box>
